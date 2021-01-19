23ffcbdd8302a9e142b5f74c004d101c7da9747a
Vanhaex <monlouis59@hotmail.fr>
2021-01-19 18:35:39 +0100

    remplissage de la classe avec les fonctions

--- src/DBaccessMySQLi.php
+++ src/DBaccessMySQLi.php
@@ -7,7 +7,10 @@ use Psr\Log\LoggerInterface;
 
 class DBaccessMySQLi implements DBaccessMySQLiInterface
 {
-    private $mysqli;    // object mysqli
+    /**
+     * @var \mysqli
+     */
+    private $mysqli;
 
     private $host;
     private $port;
@@ -15,7 +18,12 @@ class DBaccessMySQLi implements DBaccessMySQLiInterface
     private $password;
     private $database;
 
-    private $logger;    // object log
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    private $db_exceptions = true;
 
     public function initConnexion(DBaccessConnexionInterface $informations)
     {
@@ -51,7 +59,61 @@ class DBaccessMySQLi implements DBaccessMySQLiInterface
             $this->logger->error("Impossible de se connecter à la base de données. Des informations sont manquantes");
         }
 
-        
+        if($this->db_exceptions == true){
+            mysqli_report(MYSQLI_REPORT_ERROR || MYSQLI_REPORT_STRICT);
+        }
+
+        $this->mysqli->init(); // On initialise Mysqli avant toute chose
+
+        $connect = $this->mysqli->real_connect($this->host, $this->user, $this->password, $this->database, $this->port); // On peut donc se connecter
+
+        if($connect == false) {
+            $this->logger->error("Impossible de se connecter à la base de données. L'une des informations est incorrecte");
+        }
+
+    }
+
+    public function reopen()
+    {
+        if(!is_null($this->logger)){
+            $this->logger->info("Reconnexion à la base de données");
+        }
+
+        $this->mysqli->ping();
+    }
+
+    public function close()
+    {
+        if(!is_null($this->logger)){
+            $this->logger->info("Fermeture de la connexion à la base de données");
+        }
+
+        $this->mysqli->close();
+    }
+
+    public function query(string $query)
+    {
+        if(!is_null($this->logger)){
+            $this->logger->info("Execution de la requête SQL : " . $query);
+        }
+
+        trim($query); // On retire les espaces en début et en fin pour faire du ménage
+
+        if(!empty($query)){
+
+            $requete = $this->mysqli->query($query); // On peut donc executer la requête
+
+            $result = $this->mysqli->store_result();
+
+            if($requete == false){
+                $this->logger->error("Erreur lors de l'éxecution de la requête SQL");
+            }
+        }
+        else{
+            if(!is_null($this->logger)){
+                $this->logger->error("La requête n'existe pas ! Impossible donc de l'éxecuter");
+            }
+        }
     }
 }
 
