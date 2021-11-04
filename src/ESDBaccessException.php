73e645229a6bfe1ade1966b5225405b339b032a5
Vanhaex <monlouis59@hotmail.fr>
2021-11-04 16:54:42 +0100

    Create ESDBaccessException.php

---
+++ src/ESDBaccessException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Framework\ESDBaccess;
+
+use Throwable;
+
+class ESDBaccessException extends \mysqli_sql_exception
+{
+    public function __construct($message = "", $code = 0, Throwable $mysqli_exception = null)
+    {
+        parent::__construct($message, $code, $mysqli_exception);
+
+        echo "ESDBaccess error ".$code." : " . $message . " " . $mysqli_exception;
+    }
+}
