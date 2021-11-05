e1e54d281f6f0f41eab9e6fd65678e0c88d05432
Vanhaex <monlouis59@hotmail.fr>
2021-11-05 16:38:03 +0100

    ajout de fonctions et optimisation

--- src/ESDBaccess.php
+++ src/ESDBaccess.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ESDBaccess;
+namespace Framework\ESDBaccess;
 
 use mysqli;
 
@@ -15,6 +15,7 @@ class ESDBaccess implements ESDBaccessInterface
     private const MISSING_VALUES_DATABASE = -6;
     private const QUERY_SELECT_ERROR = -7;
     private const NEXT_RESULT_NOT_OBJECT = -8;
+    private const ERROR_ORDER_VALUES = -9;
 
     // Les paramètres de connexion à la bdd
     private $host;
@@ -25,7 +26,7 @@ class ESDBaccess implements ESDBaccessInterface
 
     // L'objet MySQLi
     private $sql;
-    
+
     // les variables qui sont les métadonnées des requêtes
     private $results;
     private $affectedRows;
@@ -34,7 +35,7 @@ class ESDBaccess implements ESDBaccessInterface
     /**
      * @inheritDoc
      */
-    public function connectionInformation(string $host, int $port, string $user, string $password, string $database): void
+    public function __construct(string $host, string $user, string $password, string $database, int $port)
     {
         // On vérifie d'abord si aucun paramètre n'a été oublié
         foreach (func_get_args() as $args){
@@ -91,7 +92,7 @@ class ESDBaccess implements ESDBaccessInterface
 
             // On ferme la connexion puis on vide la variable
             $this->sql->close();
-            $this->sql = NULL;
+            $this->sql = null;
 
             return true;
         }
@@ -103,50 +104,68 @@ class ESDBaccess implements ESDBaccessInterface
     /**
      * @inheritDoc
      */
-    public function querySelect(array $columns, string $table, string $condition): void
+    public function pingToDB(): bool
+    {
+        return $this->sql->ping();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function querySelect(string $table, $condition = null, array $columns = null): void
     {
         // Vérifier si les paramètres sont bien renseignés
-        if (empty($columns)){
-            throw new ESDBaccessException("Aucune colonne n'a été donnée en paramètre", self::MISSING_COLUMNS_DATABASE);
-        }
         if (!isset($table)){
             throw new ESDBaccessException("Aucune table n'a été donnée paramètre", self::MISSING_TABLE_DATABASE);
         }
 
-        // On nettoie les variables avant
-        $columns = implode(', ', array_filter(array_map('trim', $columns))); // On supprime les espaces vides puis on sépare tout par une virgule
-        $table = trim($table);
-        $condition = trim($condition);
-
-        // On prépare ensuite la requête et on ajoute la condition si elle existe
-        $query = "SELECT " . $columns . " FROM " . $table;
-        if ($condition !== ""){
-            $query .= $query . $condition;
-        }
-        
-        $query .= $query . ";";
-        
-        // On peut donc executer la requete simple
-        $execute = $this->sql->real_query($query);
-        
-        if ($execute == true){
-            // Si la requete réussie et retourne un jeu de résultat on fait quelque chose
-            if ($this->sql->field_count){
-                $results = $this->sql->store_result();
-                
-                while ($obj = $results->fetch_object()){
-                    array_push($this->results, $obj);
-                }
-                
-                $this->runOfRows = $this->sql->num_rows;
-
-                $this->results->free();
-            }
-            else {
-                $this->affectedRows = $this->sql->affected_rows;
-            }
+        // On nettoie les colonnes
+        if ($columns == null){
+            $columns = "*";
         }
         else {
+            $columns = implode(', ', array_filter(array_map('trim', $columns))); // On supprime les espaces vides puis on sépare tout par une virgule
+        }
+
+        // On nettoie aussi la table
+        $table = trim($table);
+
+        // On prépare ensuite la requête et on ajoute la condition si elle existe
+        $query = "SELECT " . htmlentities($columns) . " FROM " . htmlentities($table);
+
+        // On précise que la variable results est un array pour y stocker tous les résultats + on le vide d'abord
+        $this->results = [];
+
+        // Si la condition est renseignée, on la rajoute
+        if ($condition !== null) {
+            $condition = trim(htmlentities($condition));
+
+            $query .= $query . $condition;
+        }
+
+        $query = $query . ";"; // Sans oublier le ; qui pourrait être nécessaire
+
+        // On peut donc executer la requete simple
+        $execute = $this->sql->real_query($query);
+
+        // Si la requête est ok
+        if ($execute == true) {
+
+            if ($this->sql->field_count) {
+                $results = $this->sql->store_result();
+
+                // tant qu'on a des résultats, on va tous les ajouter un par un à la suite dans un tableau
+                while ($obj = $results->fetch_object()) {
+                    array_push($this->results, $obj);
+                }
+
+                $this->numOfRows = $results->num_rows;
+
+                $results->free();
+            } else {
+                $this->affectedRows = $this->sql->affected_rows;
+            }
+        } else {
             throw new ESDBaccessException("Une erreur a échoué pour la requête SQL suivante : " . $query, self::QUERY_SELECT_ERROR);
         }
     }
@@ -154,7 +173,7 @@ class ESDBaccess implements ESDBaccessInterface
     /**
      * @inheritDoc
      */
-    public function queryInsert(array $columns, array $values, string $table, string $condition): void
+    public function queryInsert(string $table, array $columns, array $values, $condition = null): void
     {
         // Vérifier si les paramètres sont bien renseignés
         if (empty($columns)){
@@ -167,41 +186,52 @@ class ESDBaccess implements ESDBaccessInterface
             throw new ESDBaccessException("Aucune table n'a été donnée paramètre", self::MISSING_TABLE_DATABASE);
         }
 
+        // il faut également que le nombre de colonnes renseignées soit identique au nombre de valeurs
+        if (sizeof($columns) !== sizeof($values)){
+            throw new ESDBaccessException("Le nombre de colonnes doit être identique au nombre de valeurs", self::ERROR_ORDER_VALUES);
+        }
+
         // On nettoie les variables avant
         $columns = implode(', ', array_filter(array_map('trim', $columns))); // On supprime les espaces vides puis on sépare tout par une virgule
         $values = implode(', ', array_filter(array_map('trim', $values))); // On supprime les espaces vides puis on sépare tout par une virgule
         $table = trim($table);
-        $condition = trim($condition);
-        
+
         // On prépare ensuite la requête et on ajoute la condition si elle existe
-        $query = "INSERT INTO (" . $columns . ") VALUES (" . $values . ") " . $table;
-        if ($condition !== ""){
+        $query = "INSERT INTO " . htmlentities($table) . " (" . htmlentities($columns) . ") VALUES (" . htmlentities($values) . ") ";
+
+        // On précise que la variable results est un array pour y stocker tous les résultats + on le vide d'abord
+        $this->results = [];
+
+        // Si la condition est renseignée, on la rajoute
+        if ($condition !== null) {
+            $condition = trim(htmlentities($condition));
+
             $query .= $query . $condition;
         }
-        
-        $query .= $query . ";";
-        
+
+        $query = $query . ";"; // Sans oublier le ; qui pourrait être nécessaire
+
         // On peut donc executer la requete simple
         $execute = $this->sql->real_query($query);
-        
-        if ($execute == true){
-            // Si la requete réussie et retourne un jeu de résultat on fait quelque chose
-            if ($this->sql->field_count){
+
+        // Si la requête est ok
+        if ($execute == true) {
+
+            if ($this->sql->field_count) {
                 $results = $this->sql->store_result();
-                
-                while ($obj = $results->fetch_object()){
+
+                // tant qu'on a des résultats, on va tous les ajouter un par un à la suite dans un tableau
+                while ($obj = $results->fetch_object()) {
                     array_push($this->results, $obj);
                 }
-                
-                $this->runOfRows = $this->sql->num_rows;
 
-                $this->results->free();
-            }
-            else {
+                $this->numOfRows = $results->num_rows;
+
+                $results->free();
+            } else {
                 $this->affectedRows = $this->sql->affected_rows;
             }
-        }
-        else {
+        } else {
             throw new ESDBaccessException("Une erreur a échoué pour la requête SQL suivante : " . $query, self::QUERY_SELECT_ERROR);
         }
 
@@ -213,13 +243,25 @@ class ESDBaccess implements ESDBaccessInterface
      */
     public function preparedQuerySelect(array $columns, string $table, string $condition, string $bind_type, array $bind_data): void
     {
-        // TODO: Implement preparedQuerySelect() method.
+        // Vérifier si les paramètres sont bien renseignés
+        if (empty($columns)){
+            throw new ESDBaccessException("Aucune colonne n'a été donnée en paramètre", self::MISSING_COLUMNS_DATABASE);
+        }
+        if (empty($values)){
+            throw new ESDBaccessException("Aucune valeur n'a été donnée en paramètre", self::MISSING_VALUES_DATABASE);
+        }
+        if (!isset($table)){
+            throw new ESDBaccessException("Aucune table n'a été donnée paramètre", self::MISSING_TABLE_DATABASE);
+        }
+
+        // On vérifie aussi que les types et les données ont le même nombre de données et ne sont pas vides
+
     }
 
 
     /**
-    * @inheritDoc
-    */
+     * @inheritDoc
+     */
     public function preparedQueryInsert(array $columns, array $values, string $table, string $condition, string $bind_type, array $bind_data): void
     {
         // TODO: Implement preparedQueryInsert() method.
@@ -229,7 +271,7 @@ class ESDBaccess implements ESDBaccessInterface
     /**
      * @inheritDoc
      */
-    public function allResults(): array
+    public function allResults(): ?array
     {
         return $this->results;
     }
