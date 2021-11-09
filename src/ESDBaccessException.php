64de1126b11aa23ef1dd93d451a027bc831ceff1
Vanhaex <monlouis59@hotmail.fr>
2021-11-09 16:46:17 +0100

    Update ESDBaccessException.php

--- src/ESDBaccessException.php
+++ src/ESDBaccessException.php
@@ -1,15 +1,19 @@
 <?php
 
-namespace ESDBaccess;
+namespace Framework\ESDBaccess;
 
-use Throwable;
-
-class ESDBaccessException extends \mysqli_sql_exception
+/**
+ * ESDBaccess exception handler.
+ *
+ */
+class ESDBaccessException extends \Exception
 {
-    public function __construct($message = "", $code = 0, Throwable $mysqli_exception = null)
+    public function __construct($message = "", $code = 0, \mysqli_sql_exception $mysqli_exception = null)
     {
         parent::__construct($message, $code, $mysqli_exception);
 
-        echo "ESDBaccess error ".$code." : " . $message . " " . $mysqli_exception;
+        // echo "ESDBaccess error ".$code." : " . $message . " " . $mysqli_exception;
     }
 }
+
+?>
